ca527d19944f5ab6fa132c797dca2f4fc45a452a
ubaish01 <ubaish9897@gmail.com>
2024-03-28 02:11:14 +0530

    monorepo FE and BE packages imports done from commond and Ui package

---
+++ week-16/lecture-2/apps/web/app/admin/page.tsx
@@ -0,0 +1,14 @@
+import Admin from '@repo/ui/Admin'
+import React from 'react'
+
+const page = () => {
+    return (
+        <div>
+            page
+            <Admin />
+        </div>
+
+    )
+}
+
+export default page
---
+++ week-16/lecture-2/packages/ui/src/admin.tsx
@@ -0,0 +1,5 @@
+export default function(){
+    return(
+        <div>Admin dashboard</div>
+    )
+}
